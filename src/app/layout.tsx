859c9581baa0a90cda89403c8c1fba44a97976d5
Lucky <calder07@gmail.com>
2024-03-24 22:55:41 -0300

    update

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { inter } from "@/config/fonts";
+
+import "./globals.css";
+import { Providers } from "@/components";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - Lucky | Resto",
+    default: "Home - Lucky | Resto",
+  },
+  description: "Tienda virtual de productos",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
